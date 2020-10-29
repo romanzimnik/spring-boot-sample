946b443d0cf1ff6d86c15cb6550a7a333af5e03d
rzett <rzett@software-crafter.com>
2020-10-29 11:43:28 +0100

    REST Controller implemented; Persistence Layer implemented; Custom Exceptions implemented

---
+++ src/main/java/com/softwarecrafter/springbootsample/persistence/repo/BookRepository.java
@@ -0,0 +1,10 @@
+package com.softwarecrafter.springbootsample.persistence.repo;
+
+import com.softwarecrafter.springbootsample.persistence.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findByTitle(String title);
+}
---
+++ src/main/java/com/softwarecrafter/springbootsample/web/exception/BookNotFoundException.java
@@ -0,0 +1,26 @@
+package com.softwarecrafter.springbootsample.web.exception;
+
+import com.softwarecrafter.springbootsample.persistence.model.Book;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException() {
+        super();
+    }
+
+    public BookNotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public BookNotFoundException(final String message) {
+        super(message);
+    }
+
+    public BookNotFoundException(final Throwable cause) {
+        super(cause);
+    }
+}
+
