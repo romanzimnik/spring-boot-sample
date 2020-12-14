3a292c89e442c50c724fd7778f53785301a8089c
rzett <rzett@software-crafter.com>
2020-12-14 23:55:24 +0100

    Todo Item implementation extended

--- src/main/java/com/softwarecrafter/springbootsample/persistence/repository/TodoRepository.java
+++ src/main/java/com/softwarecrafter/springbootsample/persistence/repository/TodoRepository.java
@@ -3,5 +3,5 @@ package com.softwarecrafter.springbootsample.persistence.repository;
 import com.softwarecrafter.springbootsample.persistence.model.Todo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TodoRepositiory extends MongoRepository<Todo, Long> {
+public interface TodoRepository extends MongoRepository<Todo, Long> {
 }
--- src/main/java/com/softwarecrafter/springbootsample/web/services/RepositoryTodoService.java
+++ src/main/java/com/softwarecrafter/springbootsample/web/services/RepositoryTodoService.java
@@ -1,15 +1,23 @@
 package com.softwarecrafter.springbootsample.web.services;
 
-import com.softwarecrafter.springbootsample.persistence.repository.TodoRepositiory;
+import com.softwarecrafter.springbootsample.persistence.repository.TodoRepository;
 import com.softwarecrafter.springbootsample.web.dto.TodoDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 public class RepositoryTodoService implements TodoService {
 
-    @Resource
-    private TodoRepositiory repository;
+    private static final Logger LOGGER = LoggerFactory.getLogger(RepositoryTodoService.class);
+
+    private TodoRepository repository;
+
+    @Autowired
+    RepositoryTodoService(TodoRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public List<TodoDTO> findAllTodos() {
