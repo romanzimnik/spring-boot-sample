214b05ab3c426207ec7d6014edb203a70f2f88fc
Roman Zimnik <roman@software-crafter.com>
2021-03-09 22:34:34 +0100

    note configuration refactored

--- backend/src/main/java/com/softwarecrafter/springbootsample/middleware/services/RepositoryNoteService.java
+++ backend/src/main/java/com/softwarecrafter/springbootsample/middleware/services/RepositoryNoteService.java
@@ -1,20 +1,41 @@
 package com.softwarecrafter.springbootsample.middleware.services;
 
 import com.softwarecrafter.springbootsample.middleware.dto.NoteDTO;
+import com.softwarecrafter.springbootsample.persistence.model.Note;
+import com.softwarecrafter.springbootsample.persistence.model.NoteMapper;
+import com.softwarecrafter.springbootsample.persistence.model.Todo;
+import com.softwarecrafter.springbootsample.persistence.repository.NoteRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RepositoryNoteService implements NoteService {
-    @Override
-    public List<NoteDTO> findAllNotes() {
-        return null;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RepositoryNoteService.class);
+
+    private NoteRepository repository;
+
+    @Autowired
+    RepositoryNoteService(NoteRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public NoteDTO findById(ObjectId id) {
+    public List<NoteDTO> findAllNotes() {
+
+        List<Note> notes = repository.findAll();
+
+        return NoteMapper.mapEntitiesIntoDtos(notes);
+    }
+
+    @Override
+    public Optional<NoteDTO> findById(ObjectId id) {
         return null;
     }
 
