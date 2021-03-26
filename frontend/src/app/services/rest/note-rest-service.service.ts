b0dbc8f52fe843db359edb0dee5a94f8dcfe8fd5
Roman Zimnik <roman@software-crafter.com>
2021-03-26 02:36:28 +0100

    note class implemented; note rest api service implemented

--- frontend/src/app/services/rest/note-rest-service.service.ts
+++ frontend/src/app/services/rest/note-rest-service.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Note} from "../../interfaces/note";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteRestServiceService {
 
-  constructor() { }
+  private readonly noteUrl!: string;
+
+  constructor(private http: HttpClient) {
+    this.noteUrl = 'http://localhost:8081/api/note';
+  }
+
+  /**
+   * findAll
+   */
+  public findAll(): Observable<Note[]> {
+    return this.http.get<Note[]>(this.noteUrl);
+  }
+
+
+  /**
+   * findOne
+   */
+  // TODO
+
+  /**
+   * create
+   * update
+   * @param note
+   */
+  public save(note: Note) {
+    return this.http.post<Note>(this.noteUrl, note);
+  }
+
+  /**
+   * delete
+   */
+
 }
